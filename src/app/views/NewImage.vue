30c0d47b567590d996538dfc76cdf37c42133815
huoshen80 <61808984+huoshen80@users.noreply.github.com>
2022-07-15 23:09:51 +0800

    Update NewImage.vue
    修复了Ubuntu镜像无法开基岩版服务器的问题

--- src/app/views/NewImage.vue
+++ src/app/views/NewImage.vue
@@ -225,12 +225,11 @@ WORKDIR /workspace
         this.version = "16";
       }
       if (type === 3) {
-        this.dockerFile = `FROM ubuntu:18.04
-ENV TZ=Asia/Shanghai
+        this.dockerFile = `FROM ubuntu:latest
 RUN mkdir -p /workspace
-RUN apt -y update && apt -y install openssl && DEBIAN_FRONTEND="noninteractive" apt -y install tzdata
-RUN ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
+RUN apt update
 WORKDIR /workspace
+RUN apt -y update && apt install libcurl4-openssl-dev -y && apt install wget -y && wget http://security.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1l-1ubuntu1.6_amd64.deb && dpkg -i libssl1.1_1.1.1l-1ubuntu1.6_amd64.deb
 `;
         this.name = "mcsm-ubuntu";
         this.version = "18.04";
