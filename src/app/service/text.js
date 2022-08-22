ce185d3907154cb9fb73c3d8c6400841f9f0c560
unitwk <unitwk@outlook.com>
2022-08-22 21:36:55 +0800

    Feat: Optimize text

--- src/app/service/text.js
+++
@@ -1,6 +0,0 @@
-// Copyright (C) 2022 MCSManager <mcsmanager-dev@outlook.com>
-import i18n from "../i18n";
-const $t = i18n.global.t
-
-export const ERROR_TEXT = $t("general.error");
-export const OK_TEXT = $t("general.success")
