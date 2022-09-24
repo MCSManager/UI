01ee294fd4cff62f9737d7b1e60cce7865091843
unitwk <unitwk@outlook.com>
2022-09-24 10:27:51 +0800

    Refactor: language file

--- src/app/i18n.js
+++ src/app/i18n.js
@@ -1,6 +1,6 @@
 import { createI18n } from 'vue-i18n'
-import zh_cn from "../language/zh_cn"
-import en_us from "../language/en_us"
+import zh_cn from "../language/zh_cn.json"
+import en_us from "../language/en_us.json"
 
 const i18n = createI18n({
     locale: 'en_us',
