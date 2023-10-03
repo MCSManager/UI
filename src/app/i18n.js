a2f72c8323550ed9b6c2029c942731220b4f4f49
R2kip <56793105+R2kip@users.noreply.github.com>
2023-10-04 01:04:40 +0300

    Update i18n.js

--- src/app/i18n.js
+++ src/app/i18n.js
@@ -1,6 +1,7 @@
 import { createI18n } from 'vue-i18n'
 import zh_cn from "../language/zh_cn.json"
 import en_us from "../language/en_us.json"
+import ru_ru from "../language/ru_ru.json"
 
 const i18n = createI18n({
     locale: 'en_us',
